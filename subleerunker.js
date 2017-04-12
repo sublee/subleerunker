2b9ec072078b35b0dfc37e487dff30c149bf2ffe
Heungsub Lee <sub@subl.ee>
2017-04-12 20:47:49 +0900

    Remove 1 frame from landed flame

--- subleerunker.js
+++ subleerunker.js
@@ -731,7 +731,7 @@ $.extend(Subleerunker, {
     frameRate: 0.15,
     animations: {
       burn: [[0, 0], [0, 2], [1, 2], [2, 2], [0, 3], [1, 3], [2, 3]],
-      land: [[1, 0], [2, 0], [0, 1], [1, 1]]
+      land: [[1, 0], [0, 1], [1, 1]]
     },
     defaultScene: 'burn',
 
