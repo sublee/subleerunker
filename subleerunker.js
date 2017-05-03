1f9696638798d27b8ea8ba59ad0ce0ee09641558
Heungsub Lee <sub@subl.ee>
2017-05-04 01:30:57 +0900

    Fix cut-off bug

--- subleerunker.js
+++ subleerunker.js
@@ -267,10 +267,9 @@ var GameObject = Class.$extend({
       deltaTime = time - prevTime;
       prevFrame = this.frame(this.fps, prevTime);
       // Cut off too slow delta time and frame.
-      deltaTime = limit(deltaTime, 0, 1000 / fps);
       fps = (fps === undefined ? 60 : fps);
-      var maxDeltaFrame = (frame % 2 ? Math.floor : Math.ceil)(this.fps / fps);
-      prevFrame = Math.max(frame - maxDeltaFrame, prevFrame);
+      deltaTime = limit(deltaTime, 0, 1000 / fps);
+      prevFrame = Math.max(frame - Math.ceil(this.fps / fps), prevFrame);
     }
 
     // Update this.
