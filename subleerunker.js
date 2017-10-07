75823ae45e19ca4b447272039dd24c119f0d5f72
Heungsub Lee <sub@subl.ee>
2017-10-08 00:42:39 +0900

    Count score when flame just landed before animation ends
    It makes perfect determinism.  The average scores will be increased
    slightly.

--- subleerunker.js
+++ subleerunker.js
@@ -779,33 +779,36 @@ $.extend(Subleerunker, {
     },
 
     update: function(frame) {
-      var player = this.parent.player;
-
       if (this.landed) {
+        // Already landed.  Destroy when the landing animation is done.
         if (this.hasAnimationEnded()) {
           this.destroy();
-          if (!player.dead) {
-            this.parent.upScore();
-          }
         }
         return;
       }
 
-      var prevPosition = this.position;
+      var player = this.parent.player;
 
-      var max = this.parent.height - this.height - this.landingMargin;
-      var min = this.parent.height - player.height;
-
-      if (this.position > max) {
-        this.position = max;
-        this.speed = 0;
-        this.setAnimation('land');
-        this.landed = true;
-      } else if (this.position < min) {
+      var hitboxMin = this.parent.height - player.height;
+      if (this.position < hitboxMin) {
+        // Ignore if it didn't enter into the hitbox.
         return;
       }
 
-      if (!player.dead && this.hits(player, prevPosition)) {
+      var hitboxMax = this.boundary()[1];
+      if (this.position >= hitboxMax) {
+        // Just landed.
+        this.landed = true;
+        this.setAnimation('land');
+
+        if (!player.dead) {
+          this.parent.upScore();
+        }
+        return;
+      }
+
+      if (!player.dead && this.hits(player, this.position)) {
+        // Kill the player.  The game is over.
         this.destroy();
         this.parent.gameOver();
       }
@@ -844,7 +847,10 @@ $.extend(Subleerunker, {
     maxVelocity: 10,
 
     boundary: function() {
-      return [-Infinity, this.parent.height - this.height];
+      return [
+        -Infinity,
+        this.parent.height - this.height - this.landingMargin
+      ];
     },
 
     visualize: function(state) {
