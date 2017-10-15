600f0ef0f8662f134592ab52526f17c512a5399e
Heungsub Lee <sub@subl.ee>
2017-10-16 01:49:34 +0900

    Take goalType to choose gte or eq goal score

--- solve.js
+++ solve.js
@@ -14,6 +14,10 @@ function randInt(stop) {
   return Math.floor(Math.random() * stop);
 }
 
+function extendArray(arr, tail) {
+  Array.prototype.push.apply(arr, tail);
+}
+
 function generateStream(length, prevRecord) {
   let stream = [];
 
@@ -47,13 +51,41 @@ function encodeAsReplay(randomSeed, stream) {
   return '2!' + randomSeed.toString(16) + '!' + streamWords.join('!');
 }
 
-function extendArray(arr, tail) {
-  Array.prototype.push.apply(arr, tail);
+function parseEncodedReplay(encodedReplay) {
+  let words = encodedReplay.split('!');
+
+  let version = words.shift();
+  if (version !== '2') {
+    throw new Error('only version 2 supported');
+  }
+
+  let randomSeed = parseInt(words.shift(), 16);
+
+  let stream = words.map(function(word) {
+    let deltaFrameAndInput = word.split('.');
+    let deltaFrameHex      = deltaFrameAndInput[0];
+    let inputHex           = deltaFrameAndInput[1];
+
+    let deltaFrame = parseInt(deltaFrameHex, 16);
+    let input      = parseInt(inputHex, 16);
+
+    return {deltaFrame: deltaFrame, input: input};
+  });
+
+  return {randomSeed: randomSeed, stream: stream};
 }
 
-function* solve(randomSeedOrEncodedReplay, goalScore, maxTries) {
-  var STREAM_SIZE = 10;
-  let startedAt = new Date();
+/**
+ * @param {number|string} randomSeedOrEncodedReplay
+ * @param {number} goalScore -
+ *   The score as the goal.  The solver works until it achieved the goal.
+ * @param {boolean} [goalType='gte'] -
+ *   The solving strategy.  It determines when to stop solving.
+ *   `gte` for `>=goalScore` or `eq` for `==goalScore`.
+ * @param {number} [maxTries=100]
+ */
+function* solve(randomSeedOrEncodedReplay, goalScore, goalType, maxTries) {
+  const CHUNK_SIZE = 10;
 
   function ENCODE_REPLAY(stream) {
     return encodeAsReplay(randomSeed, stream);
@@ -62,71 +94,90 @@ function* solve(randomSeedOrEncodedReplay, goalScore, maxTries) {
     return replayResult(ENCODE_REPLAY(stream));
   }
 
+  // Default arguments.
   goalScore = goalScore || 0;
+  goalType  = goalType  || 'gte';
   maxTries  = maxTries  || 100;
 
+  // Resolve the first argument.  It can be a random seed or encoded replay.
   let randomSeed;
   let stream = [];
-
   if (typeof randomSeedOrEncodedReplay === 'number') {
     randomSeed = randomSeedOrEncodedReplay;
   } else {
-    let words = randomSeedOrEncodedReplay.split('!');
-    words.shift();  // Discard version.  But it should be 2.
-    randomSeed = parseInt(words.shift(), 16);
-    let inputs = words.map(function(word) {
-      let deltaFrameAndInput = word.split('.');
-      var deltaFrameHex      = deltaFrameAndInput[0];
-      var inputHex           = deltaFrameAndInput[1];
-
-      let deltaFrame = parseInt(deltaFrameHex, 16);
-      let input      = parseInt(inputHex, 16);
-
-      return {deltaFrame: deltaFrame, input: input};
-    });
-    extendArray(stream, inputs);
+    let info = parseEncodedReplay(randomSeedOrEncodedReplay);
+    randomSeed = info.randomSeed;
+    extendArray(stream, info.stream);
   }
 
-  var score  = 0;
-  var tried  = 0;
+  // Define a success function.
+  // The solver will works while success() returns false.
+  let success;
+  switch (goalType) {
+    case 'gte':
+      success = () => score >= goalScore;
+      break;
+    case 'eq':
+      success = () => score === goalScore;
+      break;
+    default:
+      throw new Error('goalType should be "gte" or "eq"')
+  }
 
-  while (score < goalScore && tried < maxTries) {
+  let score = 0;
+  let tried = 0;
+  let startedAt = new Date();
+
+  while (true) {
     ++tried;
 
     let beforeLength = stream.length;
-    let lastRecord = stream[stream.length - 1];
+    let result;
 
-    let streamTail = generateStream(STREAM_SIZE, lastRecord);
-    extendArray(stream, streamTail);
+    if (score < goalScore) {
+      // Grow the stream to find greater score.
+      let lastRecord = stream[stream.length - 1];
+      let streamTail = generateStream(CHUNK_SIZE, lastRecord);
+      extendArray(stream, streamTail);
 
-    let result = REPLAY_RESULT(stream);
-    score = result.score;
+      // Evaluate the found score.
+      result = REPLAY_RESULT(stream);
+      score = result.score;
 
-    console.log(result);
+      // Discard records after death.
+      stream.splice(result.replayedInputs);
+    } else {
+      // Rewind to find less score.
+      stream.splice(stream.length - CHUNK_SIZE);
 
-    // Discard records after death.
-    stream.splice(result.replayedInputs);
-
-    console.log({
-      tried:     tried,
-      score:     result.score,
-      inputs:    stream.length,
-      increased: stream.length - beforeLength,
-      elapsed:   new Date() - startedAt
-    });
-    console.log(ENCODE_REPLAY(stream));
-
-    // Shake to avoid stillness.
-    if (stream.length === beforeLength) {
-      stream.pop();
+      // Evaluate the found score.
+      result = REPLAY_RESULT(stream);
+      score = result.score;
     }
 
-    yield;
-  }
+    // Report the stats of this iteration.
+    let report = {
+      tried:     tried,
+      score:     result.score,
+      replay:    ENCODE_REPLAY(stream),
 
-  return {
-    replay: ENCODE_REPLAY(stream),
-    score:  score,
-    tried:  tried
-  };
+      inputs:    stream.length,
+      increased: stream.length - beforeLength,
+
+      elapsed:   new Date() - startedAt
+    };
+
+    if (score < goalScore) {
+      // Shake to avoid stillness.
+      if (stream.length === beforeLength) {
+        stream.pop();
+      }
+    }
+
+    if (success() || tried >= maxTries) {
+      return report;
+    } else {
+      yield report;
+    }
+  }
 }
